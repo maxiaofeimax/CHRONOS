3c3cf4ca87a7ad4dca4d78036619726779b647a0
vickywu1022 <1652868153@qq.com>
2025-01-10 17:15:44 +0800

    update running scripts

--- src/model.py
+++ src/model.py
@@ -1,34 +1,42 @@
 import dashscope
-import requests
+from http import HTTPStatus
+from openai import OpenAI
 import json
-import os
+
+DASHSCOPE_API_KEY = "YOUR_API_KEY"
+OPENAI_API_KEY = "YOUR_API_KEY"
+client = OpenAI(api_key=OPENAI_API_KEY)
+
 
 def query_model(model: str, raw_prompt: str):
     if 'qwen' in model:
         responses = query_qwen(model, raw_prompt)
     elif 'gpt' in model:
         responses = query_gpt(model, raw_prompt)
-    elif 'Llama' in model:
-        responses = query_llama(model, raw_prompt)
     return responses
 
 
-def query_llama(model, prompt_str):
-    """
-    Replace with your LLAMA model's query function.
-    """
-    pass
-
-
 def query_qwen(model: str, raw_prompt: str):
-    """
-    Replace with your Qwen model's query function.
-    """
-    pass
+    response = dashscope.Generation.call(
+        api_key=DASHSCOPE_API_KEY,
+        model=model,
+        prompt=raw_prompt,
+        result_format='message',
+        use_raw_prompt=True
+    )
+    if response.status_code == HTTPStatus.OK:
+        return response.output.choices[0].message.content
+    else:
+        err = 'Error code: %s, error message: %s' % (
+            response.code,
+            response.message,
+        )
+        return err
 
 
 def query_gpt(model: str, raw_prompt: str):
-    """
-    Replace with your GPT model's query function.
-    """
-    pass
+    response = client.chat.completions.create(
+        model=model, 
+        messages={"role": "user", "content": raw_prompt}
+    )
+    return response.choices[0].message['content']
